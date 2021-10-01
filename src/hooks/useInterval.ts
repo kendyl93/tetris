7c71d2d58fe375aeeb49d5bcc68f7490f9361398
Pawel Stanecki <p.stanecki93@gmail.com>
2021-10-01 15:29:36 +0200

    init commit

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from 'react';
+
+const useInterval = (callback: any, delay: number) => {
+  const savedCallback = useRef();
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    const tick = () => {
+      if (savedCallback.current) {
+        savedCallback.current();
+      }
+    };
+
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => {
+        clearInterval(id);
+      };
+    }
+  }, [delay]);
+};
+
+export default useInterval;
