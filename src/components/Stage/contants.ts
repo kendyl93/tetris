5d2cbe8bd7e88b094b042b785bc41ff08727e944
Pawel Stanecki <p.stanecki93@gmail.com>
2021-10-04 15:45:11 +0200

    Refactor stage

--- src/components/Stage/contants.ts
+++ src/components/Stage/contants.ts
@@ -1,5 +1,8 @@
 export const CLEAN_CELL = 0;
+
+export const CELL_TYPE_INDEX = 0;
 export const CELL_STATUS_INDEX = 1;
+
 export const NO_COLLISION_WITH_ANOTHER_TETROMINO = "clear";
 
 export const STAGE = {
--- src/components/Stage/index.tsx
+++ src/components/Stage/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Cell from "../Cell";
 import { StyledStage } from "./Styles";
 import GameOver from "../GameOver";
+import { CELL_TYPE_INDEX } from "./contants";
 
 export type StageCellType = Array<number | string>;
 export type StageRowType = Array<StageCellType>;
@@ -12,15 +13,18 @@ interface Props {
   score: number;
 }
 
-const Stage: React.FC<Props> = ({ stage = [], gameOver, score }) => {
-  return (
-    <StyledStage width={stage[0]?.length} height={stage?.length}>
-      {gameOver && <GameOver score={score} />}
-      {stage.map((row) =>
-        row.map((cell, index) => <Cell key={index} type={cell[0]} />)
-      )}
-    </StyledStage>
-  );
-};
+const Stage: React.FC<Props> = ({ stage = [], gameOver, score }) => (
+  <StyledStage width={stage[0]?.length} height={stage?.length}>
+    {gameOver && <GameOver score={score} />}
+    {stage.map((row: StageRowType, rowIndex: number) =>
+      row.map((cell: StageCellType, cellIndex: number) => {
+        const cellKey = `cell-${rowIndex}-${cellIndex}`;
+        const tetrominoType = cell[CELL_TYPE_INDEX];
+
+        return <Cell key={cellKey} type={tetrominoType} />;
+      })
+    )}
+  </StyledStage>
+);
 
 export default Stage;
